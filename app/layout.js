ff169925a9a015c1b96096216594cf9cc3425c70
bontangch-wq <bontang.ch@gmail.com>
2026-09-17 06:14:30 +0800

    Add Next.js application layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+export const metadata = { title: "IMS Control Room", description: "Integrated Management System" };
+import "./globals.css";
+export default function RootLayout({ children }) { return <html lang="id"><body>{children}</body></html>; }
